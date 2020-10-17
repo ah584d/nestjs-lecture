e75a11caeb9a15f2843c5f0f5cb2b065c1a9f76d
Avraham Hamu <ah584d@intl.att.com>
2020-10-17 23:32:47 +0300

    update after presentation

--- liveExample/restAPIGraphQL/src/api/api.controller.ts
+++ liveExample/restAPIGraphQL/src/api/api.controller.ts
@@ -16,26 +16,26 @@ import { AuthGuard } from './guards/auth.guard'
 
 @Controller()
 export class ApiController {
-  constructor(private readonly apiService: ApiService) {}
+  constructor(private readonly apiService: ApiService) {} // 1. <===== DI
 
   @Get()
   getHello(): string {
     return this.apiService.getHello();
   }
 
-  @Get('/users')
+  @Get('/users')    // 2. <===== route path + decorator
   @HttpCode(200)
   getUsers(): any {
 	  return this.apiService.getUsers();
   }
 
-  @Get('/users/:id')
+  @Get('/users/:id') // 3. <===== route path with param
   @HttpCode(200)
-  getUser(@Param('id') userId): any {
+  getUser(@Param('id') userId): any {   // 4. <===== @Param
 	  return this.apiService.getUserById(userId);
   }
 
-
+  // 5 . service to db + other micro service
   @Get('users/:id/posts')
   @HttpCode(200)
   getPost(@Param('id') userId): any {
@@ -47,20 +47,26 @@ export class ApiController {
   getFollowers(@Param('id') userId): any {
 	  return this.apiService.getFollowers(userId);
   }
+   // 6. <===== DEMO
 
-  @Get('users/:id/permissions') // ---> request ---> middleware---> guard --> pipe --> Controller
+   // 7. <==== guard 
+  @Get('users/:id/permissions') // ---> request ---> middleware---> guard --> Controller --> pipe
   @UseGuards(AuthGuard) // Guards are executed after each middleware, but before any interceptor or pipe.
   @HttpCode(200)        // it return true or fale, request can pass or not
   getPermissions(@Param('id') userId): any {
 	  return this.apiService.getUserAuth(userId);
   }
 
+
+  // 8. <==== pipe 
+
+
   // 2 types of pipes:
   // transformation: transform input data to the desired form (e.g., from string to integer)
   // validation: evaluate input data and if valid, simply pass it through unchanged; otherwise, throw an exception when the data is incorrect
   @Post('/posts')
   @HttpCode(202)
-  addPhone(@Body(new ValidationPipe()) post: PostDto): any { 
+  addPhone(@Body(new ValidationPipe()) post: PostDto): any {  // 9. <====== show DTO code
 	  console.log(JSON.stringify(post));
 	  return this.apiService.addPost(post);
   }
